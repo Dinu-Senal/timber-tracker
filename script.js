74fcb7a33fcdf3a0b01ed4888f3bbc248e77b952
Shazni <60922703+Shazni78@users.noreply.github.com>
2020-04-26 12:57:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+//   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+//   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+//   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+//   faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
 
 function startVideo() {
@@ -16,12 +16,12 @@ function startVideo() {
 }
 
 video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
+  const canvas = imageapi.createCanvasFromMedia(video)
   document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const detections = await imageapi.detectAllImages(video, new imageapi.TinyImageDetectorOptions()).withImageLandmarks().withImageExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
