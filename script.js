0d25975bdde63da66a0282e0a15127b87a7cf00f
Shazni <60922703+Shazni78@users.noreply.github.com>
2020-04-26 13:45:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
-const video = document.getElementById('video')
+const imageUpload = document.getElementById('imageUpload')
 
 Promise.all([
-//   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-//   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-//   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-//   faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+  // faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  // faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  // faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+]).then(start)
 
-function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+async function start() {
+  const container = document.createElement('div')
+  container.style.position = 'relative'
+  document.body.append(container)
+  const labeledImageDescriptors = await loadLabeledImages()
+  const imageMatcher = new imageapi.ImageMatcher(labeledImageDescriptors, 0.6)
+  let image
+  let canvas
+  document.body.append('Loaded')
+  imageUpload.addEventListener('change', async () => {
+    if (image) image.remove()
+    if (canvas) canvas.remove()
+    image = await imageapi.bufferToImage(imageUpload.files[0])
+    container.append(image)
+    canvas = imageapi.createCanvasFromMedia(image)
+    container.append(canvas)
+    const displaySize = { width: image.width, height: image.height }
+    imageapi.matchDimensions(canvas, displaySize)
+    const detections = await imageapi.detectAllImages(image).withImageLandmarks().withImageDescriptors()
+    const resizedDetections = Imageapi.resizeResults(detections, displaySize)
+    const results = resizedDetections.map(d => imageMatcher.findBestMatch(d.descriptor))
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box
+      const drawBox = new imageapi.draw.DrawBox(box, { label: result.toString() })
+      drawBox.draw(canvas)
+    })
+  })
 }
 
-video.addEventListener('play', () => {
-  const canvas = imageapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
-    const detections = await imageapi.detectAllImages(video, new imageapi.TinyImageDetectorOptions()).withImageLandmarks().withImageExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
-})
+function loadLabeledImages() {
+  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+      }
+
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    })
+  )
+}
