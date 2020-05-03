6036b161c9b95386a04d3018d608e922174567e0
Navoda1234 <59735418+Navoda1234@users.noreply.github.com>
2020-05-03 19:33:20 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,46 +1,40 @@
 import React, {useState} from 'react';
 import { StyleSheet, View, Button, FlatList} from 'react-native';
 
-
-import GoalInput from './components/ViewDetails';
+import GoalInput1 from './components/ViewDetails';
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [isAddMode, setIsAddMode] = useState(false)
+  const [courseGoals1, setCourseGoals1] = useState([]);
+  const [isAddMode1, setIsAddMode1] = useState(false)
 
-  console.log(courseGoals);
-   const addGoalHandler = goalTitle =>{
+  console.log(courseGoals1);
+   const addGoalHandler1 = goalTitle =>{
     if (goalTitle.length === 0){
       return
-    }
-    //setCourseGoals([...courseGoals, enteredGoal]);
-    setCourseGoals(currentGoals => [...currentGoals, 
-    {id: Math.random().toString(), value: goalTitle}
+    }//setCourseGoals1([...courseGoals1, enteredGoal1]);
+    setCourseGoals1(currentGoals1 => [...currentGoals1, 
+    {id: Math.random().toString(), value: goalTitle1}
     ]);
-    setIsAddMode(false);
+    setIsAddMode1(false);
   };
 
-  const cancelGoalAdditionHandler = () => {
-    setIsAddMode(false);
+  const cancelGoalAdditionHandler1 = () => {
+    setIsAddMode1(false);
   }
 
   return (
     <View style={styles.screen}>
-      <Button color='#3cb371' title="View Details" onPress={() => setIsAddMode(true)} />
-      <GoalInput 
-        visible={isAddMode} 
-       
-        onCancel={cancelGoalAdditionHandler} />
+      <Button color='#3cb371' title="View Details" onPress={() => setIsAddMode1(true)} />
+      <GoalInput1 
+        visible={isAddMode1} 
+        onCancel={cancelGoalAdditionHandler1} />
 
     </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
     padding : 50
-  },
-
-  
+  }, 
 });
